ac2d275df86b3ad5c28dc8386044a30ad23a4c13
George Worsnop <georgeworsnop@Georges-MacBook-Pro.local>
2022-12-05 17:31:39 +0000

    attempt to solve day4 - unsure why it doesnt work

---
+++ day4/day4.js
@@ -0,0 +1,35 @@
+import fs from "fs";
+
+const file = fs
+  .readFileSync("day4/input.txt", { encoding: "utf-8" })
+  .trim()
+  .split("\n");
+
+let array = [];
+
+const pairs = file.map((line) => {
+  return line.split(",").map((pair) => {
+    return pair.split("-").map((number) => {
+      array.push(Number(number));
+      return number;
+    });
+  });
+});
+
+let matchTotal = 0;
+
+function overlapSolver([[firstStart, firstEnd], [secondStart, secondEnd]]) {
+  if (firstStart >= secondStart && firstEnd <= secondEnd) {
+    console.log("match1 >>>", [firstStart, firstEnd], [secondStart, secondEnd]);
+    matchTotal++;
+  } else if (secondStart >= firstStart && secondEnd <= firstEnd) {
+    console.log("match2 >>>", [secondStart, secondEnd], [firstStart, firstEnd]);
+    matchTotal++;
+  }
+}
+
+pairs.forEach((pair) => {
+  overlapSolver(pair);
+});
+
+console.log(matchTotal);
